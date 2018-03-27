10789c6be83248914c54a052b275b631893b25de
kylee <lky1001@gmail.com>
2018-03-27 18:51:11 +0900

    예제 추가

--- src/main/java/com/tistory/lky1001/boot2/example/ExampleApplication.java
+++ src/main/java/com/tistory/lky1001/boot2/example/ExampleApplication.java
@@ -2,9 +2,17 @@ package com.tistory.lky1001.boot2.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class ExampleApplication {
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExampleApplication.class, args);
